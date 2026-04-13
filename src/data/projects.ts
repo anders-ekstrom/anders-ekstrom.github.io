9e251032096fc383ba994f288c33f980ce4dc3b8
anders-ekstrom <travisanders1@gmail.com>
2026-04-12 21:57:58 -0500

    Update gallery in projects.ts to include project1

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -131,7 +131,7 @@ export const projects: Project[] = [
     longDescription: "text text more text.",
     coverImage: vert_channel_thumbnail,
     hero: dam_movie,
-    gallery: [vel,temp,spectra,upvp,uptp,vptp,tp2,dissipation],
+    gallery: [project1],
     link: "https://github.com/",
   },
 
